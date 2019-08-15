492116164594275b044fbf18bdf84e419e991957
Martijn Bruckman <martijn@oneup.company>
2019-08-15 23:16:08 +0200

    Add context menu. Add connection drawing func

--- src/components/Cyto.vue
+++ src/components/Cyto.vue
@@ -17,31 +17,36 @@ import config from "../data/cyto.json";
 import mongo from "../data/mongo.json";
 import bmc from "../data/bmc.json";
 import nodeHtmlLabel from "cytoscape-node-html-label";
+import viewUtilities from "cytoscape-view-utilities";
+import undoRedo from "cytoscape-undo-redo";
+import cxtmenu from "cytoscape-cxtmenu";
+import edgehandles from "cytoscape-edgehandles";
+
 import { constants } from "crypto";
 
 const canvas = mongo.canvas["7"];
 const assumptions = Object.values(canvas.assumptions).map(({ title, id }) => ({
   data: { id, label: title },
-  classes: ["assumption"]
+  classes: ["assumption", "defaultLabel"]
 }));
 const hypotheses = [];
 const segments = Object.values(bmc).map(({ slug, title, headline }) => ({
   data: { type: "segment", id: slug, label: title, tooltip: headline },
-  classes: ["segment"]
+  classes: ["segment", "defaultLabel"]
 }));
 
 config.style = config.style.map(style => {
-  console.log("st", style);
   if (
     style.css &&
     !style.css["border-color"] &&
     style.css["background-color"]
   ) {
-    console.log("ji");
+    // make the border color a slightly darker version of background-color
     style.css["border-color"] = chroma(style.css["background-color"])
-      .desaturate(1.2)
+      .darken(0.3)
       .hex();
   }
+
   return style;
 });
 
@@ -75,7 +80,7 @@ const edgehandlesConfig = {
   noEdgeEventsInDraw: false, // set events:no to edges during draws, prevents mouseouts on compounds
   disableBrowserGestures: true, // during an edge drawing gesture, disable browser gestures such as two-finger trackpad swipe and pinch-to-zoom
   handlePosition: function(node) {
-    return "middle top"; // sets the position of the handle in the format of "X-AXIS Y-AXIS" such as "left top", "middle top"
+    return "middle bottom"; // sets the position of the handle in the format of "X-AXIS Y-AXIS" such as "left top", "middle top"
   }
 };
 
@@ -119,10 +124,56 @@ export default {
       i: 1
     };
   },
+  created() {
+    window.addEventListener("keydown", this.keyDown);
+    window.addEventListener("keyup", this.keyUp);
+
+    window.addEventListener("keydown", this.keyDown);
+    window.addEventListener("keyup", this.keyUp);
+  },
+  beforeDestroy() {
+    window.removeEventListener("keydown", this.keyDown);
+    window.removeEventListener("keyup", this.keyUp);
+  },
   methods: {
+    keyDown(event) {
+      if (event.key === "Alt") {
+        this.eh.enable();
+        this.eh.enableDrawMode();
+        // TODO: update hover style for UX
+      }
+      if (event.key === "z" && (event.metaKey || event.ctrlKey)) {
+        this.ur.undo();
+        event.stopPropagation();
+        event.preventDefault();
+      } else if (event.key === "y" && (event.metaKey || event.ctrlKey)) {
+        this.ur.redo();
+        event.stopPropagation();
+        event.preventDefault();
+      } else if (event.key === "l" && (event.metaKey || event.ctrlKey)) {
+        this.cy.layout();
+        event.stopPropagation();
+        event.preventDefault();
+      }
+    },
+    keyUp(event) {
+      if (event.key === "Alt") {
+        this.eh.disableDrawMode();
+        this.eh.disable();
+        // TODO: remove hover style for UX
+      }
+    },
     preConfig(cytoscape) {
       // cytoscape: this is the cytoscape constructor
-      cytoscape.use(cola);
+      try {
+        cytoscape.use(cola);
+        undoRedo(cytoscape);
+        cytoscape.use(edgehandles);
+        cytoscape.use(cxtmenu);
+        // viewUtilities(cytoscape);
+      } catch (err) {
+        console.warn(err);
+      }
       //   try {
       //     nodeHtmlLabel(cytoscape);
       //   } catch (err) {
@@ -145,7 +196,43 @@ export default {
       //       } // your html template here
       //     }
       //   ]);
+
+      let eh = cy.edgehandles(edgehandlesConfig);
+      eh.disable();
+      var ur = cy.undoRedo({
+        debug: true
+      });
+      var ctxmenu = cy.cxtmenu({
+        commands: [
+          {
+            // example command (https://github.com/cytoscape/cytoscape.js-cxtmenu)
+            fillColor: "rgba(200, 200, 200, 0.75)", // optional: custom background color for item
+            content: "new connection", // html/text content to be displayed in the menu
+            contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+            select(ele) {
+              eh.enable();
+              eh.start(ele);
+              const completeHandler = (
+                event,
+                sourceNode,
+                targetNode,
+                addedEles
+              ) => {
+                let { position } = event;
+                eh.hide();
+                eh.disable();
+                // ...
+              };
+              cy.on("ehcomplete", completeHandler);
+            },
+            enabled: true // whether the command is selectable
+          }
+        ]
+      });
       this.cy = cy;
+      this.eh = eh;
+      this.ur = ur;
+      this.ctxmenu = ctxmenu;
     }
   }
 };
